f131950e9017de5e32500b9138efeec70ce32399
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-11-09 01:32:47 +0100

    style: Added typing & docstring

--- dummy_lib/core.py
+++ dummy_lib/core.py
@@ -1,5 +1,13 @@
 __all__ = ["greet_contributor"]
 
 
-def greet_contributor(name):
+def greet_contributor(name: str) -> str:
+	"""Creates a string message to greet the contributor.
+
+	Args:
+		name: name of the person to greet
+
+	Returns:
+		the greeting message
+	"""
 	return f"Hello {name}! Nice to meet you."
