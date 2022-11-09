0595a30e74abd2eaf9336d3fb0b878385a163b83
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-11-09 01:56:31 +0100

    feat: Added temperature conversion

--- dummy_lib/core.py
+++ dummy_lib/core.py
@@ -1,4 +1,6 @@
-__all__ = ["greet_contributor"]
+from typing import List
+
+__all__ = ["greet_contributor", "feature_a"]
 
 
 def greet_contributor(name: str) -> str:
@@ -11,3 +13,21 @@ def greet_contributor(name: str) -> str:
 		the greeting message
 	"""
 	return f"Hello {name}! Nice to meet you."
+
+
+def feature_a(input_temperatures: List[float]) -> List[float]:
+	"""Converts temperatures from Fahrenheit to Celsius.
+
+	Args:
+		input_temperatures: the list of temperatures in Fahrenheit
+
+	Returns:
+		the temperatures converted to Celsius
+	"""
+
+	tmp = []
+	for fahrenheit_temp in input_temperatures:
+		celsius_temp = (fahrenheit_temp - 32) * 5 / 9
+		tmp.append(celsius_temp)
+
+	return tmp
