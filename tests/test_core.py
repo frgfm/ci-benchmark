25b18c0e46f31e39e9dce484e5e92974389e7b02
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-11-09 01:56:43 +0100

    test: Updated unittests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dummy_lib.core import greet_contributor
+from dummy_lib.core import greet_contributor, feature_a
 
 
 @pytest.mark.parametrize(
@@ -12,3 +12,14 @@ from dummy_lib.core import greet_contributor
 )
 def test_greet_contributor(name, expected_output):
     assert greet_contributor(name) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_temperatures, expected_output",
+    [
+        [[], []],
+        [[50, 14, 32], [10, -10, 0]],
+    ],
+)
+def test_feature_a(input_temperatures, expected_output):
+    assert feature_a(input_temperatures) == expected_output
