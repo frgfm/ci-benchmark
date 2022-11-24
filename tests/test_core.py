05eb182431c603121fc112d11a8532afdc9f3d53
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-11-24 16:13:00 +0100

    test: Updated test cases

--- tests/test_core.py
+++ tests/test_core.py
@@ -18,8 +18,7 @@ def test_greet_contributor(name, expected_output):
     "input_temperatures, expected_output",
     [
         [[], []],
-        [[50, 14, 32], [10, -10, 0]],
-        [[32 + 9 / 5], [1.]],
+        [[50, 14., 32], [10, -10., 0]],
     ],
 )
 def test_convert_fahrenheit_to_celsius(input_temperatures, expected_output):
