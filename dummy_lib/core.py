ba4494f059c6ab325e2e8fcaddec1cb2fb6238de
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-11-24 16:26:14 +0100

    feat: Added temperature conversion feature (#10)
    * feat: Added temperature conversion
    
    * test: Updated unittests
    
    * feat: Added codeball
    
    * 'Refactored by Sourcery' (#12)
    
    Co-authored-by: Sourcery AI <>
    
    * refactor: Renamed feature_a
    
    * docs: Added header
    
    * test: Added test case
    
    * feat: Adds demo script
    
    * ci: Added CI job to run the script
    
    * test: Updated test cases
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- dummy_lib/core.py
+++ dummy_lib/core.py
@@ -1,4 +1,11 @@
-__all__ = ["greet_contributor"]
+# Copyright (C) 2022, François-Guillaume Fernandez.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
+
+from typing import List
+
+__all__ = ["greet_contributor", "convert_fahrenheit_to_celsius"]
 
 
 def greet_contributor(name: str) -> str:
@@ -11,3 +18,18 @@ def greet_contributor(name: str) -> str:
 		the greeting message
 	"""
 	return f"Hello {name}! Nice to meet you."
+
+
+def convert_fahrenheit_to_celsius(input_temperatures: List[float]) -> List[float]:
+	"""Converts temperatures from Fahrenheit to Celsius.
+
+	Args:
+		input_temperatures: the list of temperatures in Fahrenheit
+
+	Returns:
+		the temperatures converted to Celsius
+	"""
+
+	return [
+		(fahrenheit_temp - 32) * 5 / 9 for fahrenheit_temp in input_temperatures
+	]
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dummy_lib.core import greet_contributor
+from dummy_lib.core import greet_contributor, convert_fahrenheit_to_celsius
 
 
 @pytest.mark.parametrize(
@@ -12,3 +12,14 @@ from dummy_lib.core import greet_contributor
 )
 def test_greet_contributor(name, expected_output):
     assert greet_contributor(name) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_temperatures, expected_output",
+    [
+        [[], []],
+        [[50, 14., 32], [10, -10., 0]],
+    ],
+)
+def test_convert_fahrenheit_to_celsius(input_temperatures, expected_output):
+    assert convert_fahrenheit_to_celsius(input_temperatures) == expected_output
