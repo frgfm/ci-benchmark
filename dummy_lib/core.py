b0e7fbed2ee4379bb60a2081a84f2c8f9771e05f
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-11-24 16:55:36 +0100

    refactor: Renamed convert_fahrenheit_to_celsius

--- dummy_lib/core.py
+++ dummy_lib/core.py
@@ -3,9 +3,19 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
+from enum import Enum
 from typing import List
 
-__all__ = ["greet_contributor", "convert_fahrenheit_to_celsius"]
+__all__ = ["greet_contributor", "convert_temperature_sequences", "TemperatureScale"]
+
+
+class TemperatureScale(Enum):
+    """Temparature scale
+    Available values are ``celsius``, ``fahrenheit``.
+    """
+
+    CELSIUS = "celsius"
+    FAHRENHEIT = "fahrenheit"
 
 
 def greet_contributor(name: str) -> str:
@@ -20,16 +30,30 @@ def greet_contributor(name: str) -> str:
 	return f"Hello {name}! Nice to meet you."
 
 
-def convert_fahrenheit_to_celsius(input_temperatures: List[float]) -> List[float]:
-	"""Converts temperatures from Fahrenheit to Celsius.
+def convert_temperature_sequences(
+	input_temperatures: List[float],
+	input_scale: TemperatureScale = TemperatureScale.FAHRENHEIT,
+	output_scale: TemperatureScale = TemperatureScale.CELSIUS,
+) -> List[float]:
+	"""Converts temperatures from one scale to another.
 
 	Args:
-		input_temperatures: the list of temperatures in Fahrenheit
+		input_temperatures: the list of temperatures
 
 	Returns:
-		the temperatures converted to Celsius
+		the temperatures converted to the expected scale
 	"""
 
-	return [
-		(fahrenheit_temp - 32) * 5 / 9 for fahrenheit_temp in input_temperatures
-	]
+	if input_scale == output_scale:
+		return input_temperatures
+
+	if input_scale == TemperatureScale.FAHRENHEIT and output_scale == TemperatureScale.CELSIUS:
+		return [
+			(fahrenheit_temp - 32) * 5 / 9 for fahrenheit_temp in input_temperatures
+		]
+	elif input_scale == TemperatureScale.CELSIUS and output_scale == TemperatureScale.FAHRENHEIT:
+		return [
+			celsius_temp * 9 / 5 + 32 for celsius_temp in input_temperatures
+		]
+
+	raise NotImplementedError
