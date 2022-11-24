c9dd4e3103693ef288433cdbdc3349889acd3aec
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-11-24 16:58:35 +0100

    style: Fixed formatting

--- dummy_lib/core.py
+++ dummy_lib/core.py
@@ -19,41 +19,37 @@ class TemperatureScale(Enum):
 
 
 def greet_contributor(name: str) -> str:
-	"""Creates a string message to greet the contributor.
+    """Creates a string message to greet the contributor.
 
-	Args:
-		name: name of the person to greet
+    Args:
+            name: name of the person to greet
 
-	Returns:
-		the greeting message
-	"""
-	return f"Hello {name}! Nice to meet you."
+    Returns:
+            the greeting message
+    """
+    return f"Hello {name}! Nice to meet you."
 
 
 def convert_temperature_sequences(
-	input_temperatures: List[float],
-	input_scale: TemperatureScale = TemperatureScale.FAHRENHEIT,
-	output_scale: TemperatureScale = TemperatureScale.CELSIUS,
+    input_temperatures: List[float],
+    input_scale: TemperatureScale = TemperatureScale.FAHRENHEIT,
+    output_scale: TemperatureScale = TemperatureScale.CELSIUS,
 ) -> List[float]:
-	"""Converts temperatures from one scale to another.
+    """Converts temperatures from one scale to another.
 
-	Args:
-		input_temperatures: the list of temperatures
+    Args:
+            input_temperatures: the list of temperatures
 
-	Returns:
-		the temperatures converted to the expected scale
-	"""
+    Returns:
+            the temperatures converted to the expected scale
+    """
 
-	if input_scale == output_scale:
-		return input_temperatures
+    if input_scale == output_scale:
+        return input_temperatures
 
-	if input_scale == TemperatureScale.FAHRENHEIT and output_scale == TemperatureScale.CELSIUS:
-		return [
-			(fahrenheit_temp - 32) * 5 / 9 for fahrenheit_temp in input_temperatures
-		]
-	elif input_scale == TemperatureScale.CELSIUS and output_scale == TemperatureScale.FAHRENHEIT:
-		return [
-			celsius_temp * 9 / 5 + 32 for celsius_temp in input_temperatures
-		]
+    if input_scale == TemperatureScale.FAHRENHEIT and output_scale == TemperatureScale.CELSIUS:
+        return [(fahrenheit_temp - 32) * 5 / 9 for fahrenheit_temp in input_temperatures]
+    elif input_scale == TemperatureScale.CELSIUS and output_scale == TemperatureScale.FAHRENHEIT:
+        return [celsius_temp * 9 / 5 + 32 for celsius_temp in input_temperatures]
 
-	raise NotImplementedError
+    raise NotImplementedError
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dummy_lib.core import greet_contributor, convert_temperature_sequences, TemperatureScale
+from dummy_lib.core import TemperatureScale, convert_temperature_sequences, greet_contributor
 
 
 @pytest.mark.parametrize(
@@ -19,14 +19,32 @@ def test_greet_contributor(name, expected_output):
     [
         [[], None, None, None, []],
         # Fahrenheit to Celsius
-        [[50, 14., 32], None, None, None, [10, -10., 0]],
-        [[50, 14., 32], TemperatureScale.FAHRENHEIT, TemperatureScale.CELSIUS, None, [10, -10., 0]],
+        [[50, 14.0, 32], None, None, None, [10, -10.0, 0]],
+        [
+            [50, 14.0, 32],
+            TemperatureScale.FAHRENHEIT,
+            TemperatureScale.CELSIUS,
+            None,
+            [10, -10.0, 0],
+        ],
         # Celsius to Fahrenheit
-        [[10, -10., 0], TemperatureScale.CELSIUS, TemperatureScale.FAHRENHEIT, None, [50, 14., 32]],
+        [
+            [10, -10.0, 0],
+            TemperatureScale.CELSIUS,
+            TemperatureScale.FAHRENHEIT,
+            None,
+            [50, 14.0, 32],
+        ],
         # Same scale
-        [[10, -10., 0], TemperatureScale.CELSIUS, TemperatureScale.CELSIUS, None, [10, -10., 0]],
+        [
+            [10, -10.0, 0],
+            TemperatureScale.CELSIUS,
+            TemperatureScale.CELSIUS,
+            None,
+            [10, -10.0, 0],
+        ],
         # Error
-        [[10, -10., 0], TemperatureScale.CELSIUS, "kelvin", NotImplementedError, None],
+        [[10, -10.0, 0], TemperatureScale.CELSIUS, "kelvin", NotImplementedError, None],
     ],
 )
 def test_convert_temperature_sequences(input_temperatures, input_scale, output_scale, error_type, expected_output):
